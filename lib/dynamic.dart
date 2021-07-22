afb2de44ff84887a49c44f3333cdc42a1b94ef0d
李志刚 <zhigang.li@oriente.com>
2021-07-22 10:40:01 +0800

    feat: add image_picker form camear and gallery

--- lib/dynamic.dart
+++ lib/dynamic.dart
@@ -1,9 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-class DynamicPage extends StatelessWidget {
+class DynamicPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => DynamicImplPage();
+}
+
+class DynamicImplPage extends State<DynamicPage> {
   List<Image> imgs = [
     //建立了一个图片数组
     Image.network(
@@ -19,53 +26,66 @@ class DynamicPage extends StatelessWidget {
       fit: BoxFit.cover,
     ),
   ];
-  final ImagePicker _picker =  ImagePicker();
+  final ImagePicker _picker = ImagePicker();
+  var fileFromCamera;
+
+  Widget _ImageView(XFile xfile) {
+    if (xfile == null) {
+      return Center(
+        child: Text("请选择图片或者拍照"),
+      );
+    } else {
+      return Image.file(File(xfile.path), width: 200, height: 200);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
-          children: [
-            new Container(
-              width: 400,
-              height: 220,
-              child: new Swiper(
-                itemBuilder: (BuildContext context, int index) {
-                  return imgs[index];
-                },
-                itemCount: 3,
-                pagination: new SwiperPagination(),
-                control: new SwiperControl(),
-                autoplay: true,
-                outer: false,
-
-              ),
-            ),
-            new OutlinedButton(
-                onPressed: () async{
-                  var xFile = await _picker.pickImage(source: ImageSource.camera);
-
-                },
-                child: Text("拍照"),
-              style: OutlinedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0)
-                ),
-                side: BorderSide(width: 2,color: Colors.red)
-              ),
-            ),new OutlinedButton(
-              onPressed: (){
-
-              },
-              child: Text("相册"),
-              style: OutlinedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0)
-                  ),
-                  side: BorderSide(width: 2,color: Colors.red)
-              ),
-            )
-          ],
+      children: [
+        new Container(
+          width: 400,
+          height: 220,
+          child: new Swiper(
+            itemBuilder: (BuildContext context, int index) {
+              return imgs[index];
+            },
+            itemCount: 3,
+            pagination: new SwiperPagination(),
+            control: new SwiperControl(),
+            autoplay: true,
+            outer: false,
+          ),
+        ),
+        _ImageView(fileFromCamera),
+        new OutlinedButton(
+          onPressed: () async {
+            var xFile = await _picker.pickImage(source: ImageSource.camera);
+            setState(() {
+              fileFromCamera = xFile!;
+            });
+          },
+          child: Text("拍照"),
+          style: OutlinedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0)),
+              side: BorderSide(width: 2, color: Colors.red)),
+        ),
+        new OutlinedButton(
+          onPressed: () async {
+            var xFile = await _picker.pickImage(source: ImageSource.gallery);
+            setState(() {
+              fileFromCamera = xFile!;
+            });
+          },
+          child: Text("相册"),
+          style: OutlinedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0)),
+              side: BorderSide(width: 2, color: Colors.red)),
         )
-    );
+      ],
+    ));
   }
 }
